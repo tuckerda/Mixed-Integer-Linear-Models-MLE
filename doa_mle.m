ef3c910e9d864afe8231ceec8c20e89c3a43612f
David Tucker <93163653+tuckerda@users.noreply.github.com>
2023-07-14 17:31:12 -0400

    Update doa_mle.m

--- doa_mle.m
+++ doa_mle.m
@@ -44,22 +44,3 @@ for k = 1:Nres
     end
 end
 end
-
-% grid_res = [ 12e-3, 4e-3, 1e-3];
-% omega_scan = cell([length(grid_res),1]);
-% for k=1:length(grid_res)
-%     if k==1
-%         omega_bounds = [-ones([n,1]), ones([n,1])];
-%     else
-%         omega_bounds = [-grid_res(k-1)*ones([n,1]), grid_res(k-1)*ones([n,1])];
-%     end
-%     omega_grid = cell(n,1);
-%     for i = 1:n
-%         omega_grid(i) = { linspace( omega_bounds(i,1), omega_bounds(i,2), ceil( ( omega_bounds(i,2) - omega_bounds(i,1))/grid_res(k) + 1 ) ) };
-%     end
-%     tmp = cell(size(omega_grid));
-%     [tmp{:}] = ndgrid(omega_grid{:});
-%     tmp = reshape(cat(n+1,tmp{:}),[],n).';
-%     tmp(:, vecnorm(tmp,2,1) > 1 ) = [];
-%     omega_scan{k} = tmp;
-% end
